620c6a00c55c80a2047c9329b4bdc3afc8487a7c
kjm90110 <kjm90118730@gmail.com>
2025-11-24 11:20:54 +0900

    mysql setting

---
+++ db/database.mjs
@@ -0,0 +1,12 @@
+import mysql from "mysql2";
+import { config } from "../config.mjs";
+
+// db와 connection 해놓는 객체
+const pool = mysql.createPool({
+    host: config.db.host,
+    user: config.db.user,
+    database: config.db.database,
+    password: config.db.password,
+});
+
+export const db = pool.promise(); // 비동기로 return 하도록 promise로 내보냄
